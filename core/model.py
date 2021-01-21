5059aff7c8cb25560450821547041e429be977fa
Wxin96 <18706306187@163.com>
2021-01-21 19:15:09 +0800

    creat_project

---
+++ core/model.py
@@ -0,0 +1,17 @@
+from keras.models import Sequential, load_model
+
+
+class Model():
+    """A class for an building and inferencing an lstm model"""
+
+    def __init__(self):
+        self.model = Sequential()
+
+    def build_model(self):
+        self.model
+
+    def train(self, x, y, epochs, batch_size, save_dir):
+        x
+
+
+
