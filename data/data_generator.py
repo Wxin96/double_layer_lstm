6457ea80aae0fc516877ae38ce85f3559b20a795
Wxin96 <18706306187@163.com>
2021-01-28 16:34:03 +0800

    仿真数据模块完成

--- data/data_generator.py
+++ data/data_generator.py
@@ -176,17 +176,59 @@ def draw_trajectory(traj: np.ndarray):
     plt.show()
 
 
-def generator_3d_ranging_data(traj: np.ndarray, anchor_location: np.ndarray, origin_coordinate: np.ndarray,
-
-                              mode: int = 0, nlos_prob: float = 0.2) -> np.ndarray:
+def generator_3d_ranging_data(traj: np.ndarray, anchors_location: np.ndarray, origin_coordinate: np.ndarray,
+                              los_sd: float, nlos_bias: float, nlos_sd: float,
+                              mode: int = 0, nlos_prob: float = 0.2) -> (np.ndarray, np.ndarray):
     """
     根据轨迹生成基站测距数据。
+    unit: m
     Args:
         traj: 轨迹信息，维度（time_step, 3）, time_step-步长
-        anchor_location: 三维基站坐标，维度（num_anchor, 3）, num_anchor-基站个数
-        origin_coordinate:
-        mode:
-        nlos_prob:
+        anchors_location: 三维基站坐标，维度（num_anchor, 3）, num_anchor-基站个数
+        origin_coordinate: 坐标原点在三维空间的位置，进行坐标平移
+        los_sd: LOS环境下标准差
+        nlos_bias: nlos环境下偏差
+        nlos_sd: nlos环境下标准差
+        mode:  0-los条件，1-nlos环境(根据概率，基站有可能出现nlos情况)
+        nlos_prob: nlos出现的概率
     Returns:
-        基站测距数据，维数（time_step, num_anchor）
+        基站测距数据，维数（time_step, num_anchor）,坐标平移后的轨迹坐标
     """
+    # check
+    assert traj.shape == (len(traj), 3)
+    assert anchors_location.shape == (len(anchors_location), 3)
+    assert origin_coordinate.shape == (3,)
+    assert los_sd >= 0 and nlos_sd >= 0 and 0 <= nlos_prob <= 1
+    # init
+    ranging_data = np.zeros(shape=(len(traj), len(anchors_location)))
+    # coordinate translation
+    traj -= origin_coordinate
+    # generator 3d ranging data
+    for traj_idx in range(len(traj)):
+        for anchor_idx in range(len(anchors_location)):
+            if mode == 1:
+                ranging_data[traj_idx][anchor_idx] = \
+                    generator_single_ranging(anchors_location[anchor_idx], traj[traj_idx], nlos_bias, nlos_sd) \
+                        if random.random() < nlos_prob \
+                        else generator_single_ranging(anchors_location[anchor_idx], traj[traj_idx], 0, los_sd)
+            else:
+                ranging_data[traj_idx][anchor_idx] = \
+                    generator_single_ranging(anchors_location[anchor_idx], traj[traj_idx], 0, los_sd)
+    return ranging_data, traj
+
+
+def generator_single_ranging(anchor_loc: np.ndarray, tag_loc: np.ndarray, bias: float, sd: float) -> float:
+    """
+    根据基站和标签位置，添加偏差和正态分布随机误差，模拟生成测距数据.
+    参数的单位，unit:m
+    :param anchor_loc: 基站位置，向量维度-（3,）
+    :param tag_loc: 标签位置，向量维度-（3,）
+    :param bias: nlos偏置误差
+    :param sd: 正态分布的标准差
+    :return: 单次测距距离,unit: m
+    """
+    assert anchor_loc.shape == (3,)
+    assert tag_loc.shape == (3,)
+    dist = np.linalg.norm(anchor_loc - tag_loc)
+    dist += random.gauss(bias, sd)
+    return dist
--- data/test_data_generator.py
+++ data/test_data_generator.py
@@ -10,6 +10,7 @@
 """
 from unittest import TestCase
 from data import data_generator
+import numpy as np
 
 
 class Test(TestCase):
@@ -20,3 +21,22 @@ class Test(TestCase):
     def test_draw_trajectory(self):
         traj = data_generator.generator_3d_trajectory(1000, step_mode=1, random_loc=True, z_high=3)
         data_generator.draw_trajectory(traj)
+
+    def test_generator_single_ranging(self):
+        anchor_loc = np.array([0, 0, 0])
+        tag_loc = np.array([3, 4, 12])
+        nlos_dist = data_generator.generator_single_ranging(anchor_loc, tag_loc, 0.5, 45e-3)
+        print(nlos_dist)
+
+    def test_generator_3d_ranging_data(self):
+        anchors_loc_list = [[0.0, 0.0, 0.0], [0.0, 5.0, 4.0], [4.0, 5.0, 12.0], [10.0, 5.0, 4.0]]
+        anchors_loc = np.array(anchors_loc_list)
+        origin_coordinate = np.array([2.5, 1.5, 0])
+        traj = data_generator.generator_3d_trajectory(10000, step_mode=1, random_loc=True, z_high=3)
+        ranging_data,traj = data_generator.generator_3d_ranging_data(traj, anchors_loc, origin_coordinate,
+                                                                     20e-3, 0.6, 45e-3, 1, 0.4)
+        np.set_printoptions(threshold=np.inf)
+        print(ranging_data)
+        print(traj)
+        print(ranging_data.shape)
+        print(traj.shape)
